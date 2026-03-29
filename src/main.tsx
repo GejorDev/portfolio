155f459f0d8177d7b16735889de004755c93b1f5
GejorDev <jorgediegoalonso12@gmail.com>
2026-03-29 17:01:30 +0200

    feat: add vercel analytics and speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { I18nextProvider } from 'react-i18next'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import i18n from './i18n/index'
 import './styles/globals.css'
 
@@ -10,6 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
       <App />
+      <SpeedInsights />
+      <Analytics />
     </I18nextProvider>
   </React.StrictMode>,
 )
